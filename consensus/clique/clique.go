e575bf9bdc537b323ae7abdfee9561df817b6741
iroan-intel <iroanuc@gmail.com>
2024-02-29 11:36:58 +0800

    disable IsShanghai

--- consensus/clique/clique.go
+++ consensus/clique/clique.go
@@ -299,9 +299,9 @@ func (c *Clique) verifyHeader(chain consensus.ChainHeaderReader, header *types.H
 	if header.GasLimit > params.MaxGasLimit {
 		return fmt.Errorf("invalid gasLimit: have %v, max %v", header.GasLimit, params.MaxGasLimit)
 	}
-	if chain.Config().IsShanghai(header.Number, header.Time) {
-		return errors.New("clique does not support shanghai fork")
-	}
+	// if chain.Config().IsShanghai(header.Number, header.Time) {
+	// 	return errors.New("clique does not support shanghai fork")
+	// }
 	// Verify the non-existence of withdrawalsHash.
 	if header.WithdrawalsHash != nil {
 		return fmt.Errorf("invalid withdrawalsHash: have %x, expected nil", header.WithdrawalsHash)
